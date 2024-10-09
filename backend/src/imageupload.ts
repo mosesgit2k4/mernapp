e2f14042bc5664948e47e884c1a0aaaf286ea132
mosesgit2k4 <166894011+mosesgit2k4@users.noreply.github.com>
2024-10-09 11:30:02 +0530

    added validation to image

--- backend/src/imageupload.ts
+++ backend/src/imageupload.ts
@@ -1,13 +1,32 @@
-import multer from 'multer'
-import path from 'path'
+import multer, { FileFilterCallback } from 'multer';
+import path from 'path';
+import { Request } from 'express';
+
 const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'public/images')
-    },
-    filename: (req,file,cb)=>{
-        cb(null,file.fieldname + "_" + Date.now() + path.extname(file.originalname))
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+  const filetypes = /jpeg|jpg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error('Invalid file type. Only JPEG, JPG, and PNG files are allowed.'));
+  }
+};
+
 export const upload = multer({
-    storage:storage
-}).single('image')
+  storage: storage,
+  limits: {
+    fileSize: 2 * 1024 * 1024 
+  },
+  fileFilter: fileFilter
+}).single('image');
