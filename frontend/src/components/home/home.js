63fe4d9bae3c8205755733c3439ef16f9469f19f
mosesgit2k4 <166894011+mosesgit2k4@users.noreply.github.com>
2024-10-10 15:19:37 +0530

    Plan card design

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,42 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import './home.css'
+import './home.css';
+
 function Homepage() {
-    const navigator = useNavigate()
-    const [plans, setplan] = useState([])
+    const navigate = useNavigate();
+    const [plans, setPlans] = useState([]);
+
     useEffect(() => {
         fetch('/api/usermanagement/plans', {
-            method: "Get",
-        }).then(response => {
-            return response.json()
-        }).then(data => {
-            setplan(data)
-        })
-    }, [])
-    function subscriptiontoplan(plan) {
-       navigator("/subscription",{state:{plan}})
+            method: "GET",
+        }).then(response => response.json())
+          .then(data => setPlans(data));
+    }, []);
+
+    function subscriptionToPlan(plan) {
+        navigate("/subscription", { state: { plan } });
     }
+
     return (
-        <div>
+        <div className="container">
             <h1>Plans</h1>
-            <div className="d-flex flex row justify-content-start">
+            <div className="plans-grid">
                 {plans.map(plan => (
-                    <li key={plan.id} className="list-style">
-                        <div className="card">
-                            <div className="content">
-                                <div className="title">{plan.name}</div>
-                                <div className="price">
-                                    <img src ={plan.image} width={100} height={100} alt=""/>
-                                </div>
-                                <div className="description">{plan.description}</div>
+                    <div key={plan.id} className="card">
+                        <div className="content">
+                            <div className="title">{plan.name}</div>
+                            <div className="price">
+                                <img src={plan.image} width={100} height={100} alt={plan.name} />
                             </div>
-                            <button className="button type1"onClick={() => subscriptiontoplan(plan)}></button>
-                            </div>
-                    </li>
+                            <div className="description">{plan.description}</div>
+                        </div>
+                        <button className="button type1" onClick={() => subscriptionToPlan(plan)}></button>
+                    </div>
                 ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default Homepage
+export default Homepage;
