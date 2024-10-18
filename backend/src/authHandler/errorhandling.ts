924580f2beb11376f8caca22ee626290d3bac4fd
mosesgit2k4 <166894011+mosesgit2k4@users.noreply.github.com>
2024-10-18 16:47:45 +0530

    payment success and payment failed message

--- backend/src/authHandler/errorhandling.ts
+++ backend/src/authHandler/errorhandling.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
 const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error("Error Handler:", err.message || err);
-
     res.status(err.status || 500).json({
         message: err.message || "Internal Server Error",
     });
