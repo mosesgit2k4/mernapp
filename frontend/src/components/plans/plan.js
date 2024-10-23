41ffc879dc3237e91441439d07d6b8c4632c51e8
mosesgit2k4 <166894011+mosesgit2k4@users.noreply.github.com>
2024-10-23 15:28:51 +0530

    added admin dashboard in viewdetails

--- frontend/src/components/plans/plan.js
+++ frontend/src/components/plans/plan.js
@@ -85,7 +85,7 @@ function Adminplan() {
                     <Navbar.Brand>Admin</Navbar.Brand>
                     <Nav className="flex-column mt-4">
                         <Nav.Link as={Link} to="/adminplan"> Add Plans</Nav.Link>
-                        <Nav.Link as={Link} to="/users">User</Nav.Link>
+                        <Nav.Link as={Link} to="/users">Users</Nav.Link>
                         <Nav.Link as={Link} to="/plandetails">Plans</Nav.Link>
                     </Nav>
                     <Dropdown className="mt-auto dropup">
